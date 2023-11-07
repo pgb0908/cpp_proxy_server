81d80ea1c16daf62be99e4f916f33531b712b0d9
gyubong_park <pgb8909@naver.com>
2023-11-08 02:44:45 +0900

    http_parser 헤더 파싱 처리

--- src/network/Session.cpp
+++ src/network/Session.cpp
@@ -5,6 +5,25 @@
 #include "Session.h"
 #include <iostream>
 
+Session::Session(tcp::socket socket)
+        : socket_(std::move(socket)){
+
+    settings.on_message_begin = on_message_begin;
+    settings.on_header_field      = on_header_field;
+    settings.on_header_value      = on_header_value;
+    //settings.on_path              = request_path_cb;
+    //settings.on_uri               = request_uri_cb;
+    //settings.on_fragment          = fragment_cb;
+    //settings.on_query_string      = query_string_cb;
+    //settings.on_body              = body_cb;
+    //settings.on_headers_complete  = headers_complete_cb;
+    settings.on_message_complete  = message_complete_cb;
+
+    parser_ = std::make_unique<http_parser>();
+    http_parser_init(parser_.get(), HTTP_REQUEST);
+    parser_->data = &http_data_;
+}
+
 
 void Session::read() {
     auto self(shared_from_this());
@@ -20,20 +39,27 @@ void Session::read() {
                                            std::istreambuf_iterator<char>()
                                    };
 
-                                   auto nparsed = http_parser_execute(parser.get(),
+                                   auto nparsed = http_parser_execute(parser_.get(),
                                                                       &settings,
                                                                       data.data(),
                                                                       data.size());
 
 
-                                   if (parser->upgrade) {
+                                   if (parser_->upgrade) {
                                        /* handle new protocol */
                                    }
 
+                                   auto* user_data = static_cast<struct my_http_data*>(parser_->data);
+                                   user_data->header_fields;
+
+
                                    // we just print the data, you can here call other api's
                                    // or whatever the server needs to do with the received data
-                                   std::cout << data << std::endl << std::endl;
-                                   //wait_for_request();
+                                   //std::cout << data << std::endl << std::endl;
+
+                                   // todo something write
+
+                                   read();
                                } else {
                                    std::cout << "error: " << ec << std::endl;;
                                }
@@ -53,3 +79,5 @@ void Session::write(std::size_t length) {
                           }
                       });
 }
+
+
--- src/network/Session.h
+++ src/network/Session.h
@@ -8,28 +8,22 @@
 #include <memory>
 #include <asio.hpp>
 #include <http_parser.h>
+#include <stack>
 
 using namespace asio::ip;
+
+#define MAX_HEADERS 10
+#define MAX_ELEMENT_SIZE 500
+
 
 // this was created as shared ptr and we need later `this`
 // therefore we need to inherit from enable_shared_from_this
 class Session : public std::enable_shared_from_this<Session>
 {
 public:
-    // our sesseion holds the socket
-    Session(tcp::socket socket)
-            : socket_(std::move(socket)) {
+    // our sesseion holds the pParser
+    explicit Session(tcp::socket pParser);
 
-
-        settings.on_url = [](http_parser*, const char *at, size_t length)->int{ return 0;};
-        settings.on_header_field = [](http_parser*, const char *at, size_t length)->int{ return 0;};
-
-        parser = std::make_unique<http_parser>();
-        http_parser_init(parser.get(), HTTP_REQUEST);
-        parser->data = &socket_;
-
-
-    }
 
     // and run was already called in our server, where we just wait for requests
     void run() {
@@ -43,11 +37,70 @@ private:
     tcp::socket socket_;
     asio::streambuf m_buffer;
     enum { max_length = 1024 };
-    char data_[max_length];
+    char data_[max_length]{};
 
     // https://github.com/nodejs/http-parser
-    http_parser_settings settings;
-    std::unique_ptr<http_parser> parser;
+    http_parser_settings settings{};
+    std::unique_ptr<http_parser> parser_;
+
+    struct my_http_data {
+        char method[64];
+        //char header_fields[MAX_HEADERS][1024];
+        std::stack<std::pair<std::string, std::string>> header_fields;
+        int current_header_index;
+    };
+
+    my_http_data http_data_;
+
+    static int on_message_begin(http_parser* parser){
+        //printf("HTTP 메시지 시작\n");
+        auto* user_data = (struct my_http_data*)parser->data;
+        user_data->current_header_index = 0;
+        return 0;
+    }
+
+    // HTTP 메서드가 파싱될 때 호출되는 콜백
+    static int on_url(http_parser* parser, const char *at, size_t length) {
+        //printf("메서드: %.*s\n", (int)length, at);
+        auto* user_data = (struct my_http_data*)parser->data;
+        strncpy(user_data->method, at, length);
+        user_data->method[length] = '\0';
+        return 0;
+    }
+
+    // HTTP 헤더가 파싱될 때 호출되는 콜백
+    static int on_header_field(http_parser* parser, const char *at, size_t length) {
+        struct my_http_data* user_data = (struct my_http_data*)parser->data;
+        if (user_data->current_header_index < MAX_HEADERS) {
+            std::string field(at, length);
+            user_data->header_fields.emplace(field, "");
+
+            user_data->current_header_index++;
+        }
+        return 0;
+    }
+
+    // HTTP 헤더 값이 파싱될 때 호출되는 콜백
+    static int on_header_value(http_parser* parser, const char *at, size_t length) {
+        //printf("헤더 값: %.*s\n", (int)length, at);
+        struct my_http_data* user_data = (struct my_http_data*)parser->data;
+        if (user_data->current_header_index < MAX_HEADERS) {
+            std::string field_data(at, length);
+            auto front = user_data->header_fields.top();
+            auto field = front.first;
+            user_data->header_fields.pop();
+            user_data->header_fields.emplace(field, field_data);
+
+            user_data->current_header_index++;
+        }
+        return 0;
+    }
+
+
+    static int message_complete_cb(http_parser* parser){
+        //printf("HTTP 메시지 완료\n");
+        return 0;
+    }
 
 };
 
